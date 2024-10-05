fba9d26c5888ecdad6806b90a6b20766b5729541
sainn30 <sainn3008@gmail.com>
2024-10-05 21:26:54 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Toggle & reponsive nav
+
+const navSlide = () =>{
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener ("click", () => {
+        navList.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger")
+    })
+}
+
+navSlide();
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByName("form")) {
+        form.reset()
+    }
+}
